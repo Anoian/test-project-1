06327cd05c869a0cbb07d956af6e8e10d5509298
Fabian Letsch <28201880+Anoian@users.noreply.github.com>
2023-05-28 21:03:22 +0200

    refactor: add examples to App for TemperatureTile

--- src/components/App/index.test.tsx
+++ src/components/App/index.test.tsx
@@ -4,6 +4,6 @@ import App from '.';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = screen.getByTestId('wrapper');
+  expect(wrapper).toBeInTheDocument();
 });
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,55 @@
 import React from 'react';
-import './index.css';
+import styles from './index.module.css';
+import TemperatureTile from 'components/TemperatureTile';
 
 function App() {
   return (
-    <div className="App">
-      TestProject
+    <div className={styles.app} data-testid="wrapper">
+      <TemperatureTile
+        minValue={-40}
+        maxValue={200}
+        value={-40}
+      />
+      <TemperatureTile
+        minValue={0}
+        maxValue={100}
+        value={22}
+      />
+      <TemperatureTile
+        minValue={-1000}
+        maxValue={1000}
+        value={0}
+      />
+      <TemperatureTile
+        minValue={10}
+        maxValue={0}
+        value={5}
+      />
+      <TemperatureTile
+        minValue={1}
+        maxValue={1}
+        value={0}
+      />
+      <TemperatureTile
+        minValue={0}
+        maxValue={1}
+        value={0}
+      />
+      <TemperatureTile
+        minValue={0}
+        maxValue={1}
+        value={1}
+      />
+      <TemperatureTile
+        minValue={0}
+        maxValue={100}
+        value={200}
+      />
+      <TemperatureTile
+        minValue={0}
+        maxValue={100}
+        value={66}
+      />
     </div>
   );
 }
